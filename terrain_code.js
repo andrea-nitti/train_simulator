d62a2a0ed0a03f1624d6cfac4174b51062fe9a5f
Andrea Nitti <andrea.nitti2201@gmail.com>
2021-03-16 20:14:13 +0100

    Clean-up

--- terrain_code.js
+++ terrain_code.js
@@ -6,76 +6,12 @@ function createTerrain(scene) {
     for(let i=0; i<10; i++) {   //numero di chunk da generare per ogni segmento
         z_offset = i * chunk_size;
         
-        //creazione binari
-        /*for(let x_offset=-8; x_offset<=8; x_offset+=16) {   //il valore di x_offset varia la distanza fra i centri dei binari
-            for(let k=-2; k<=2; k+=4) {
-                let rail_h_inf = BABYLON.MeshBuilder.CreateBox('rail_h_inf', {height: 0.1, depth: chunk_size, width: 1.0}, scene);   //h_inf indica la parte orizzontale inferiore di una rotaia
-                rail_h_inf.material = metal;
-                rail_h_inf.position.y = -0.5;
-                rail_h_inf.position.x = k + x_offset;
-                rail_h_inf.position.z = z_offset;
-                rail_h_inf.setParent(parent_mesh);
-            }
-            for(let i=-2; i<=2; i+=4) {
-                let rail_v = BABYLON.MeshBuilder.CreateBox('rail_v', {height: 1.0, depth: chunk_size, width: 0.25}, scene);   //v indica la parte verticale di una rotaia
-                rail_v.material = metal;
-                rail_v.position.y = 0;
-                rail_v.position.x = i + x_offset;
-                rail_v.position.z = z_offset;
-                rail_v.setParent(parent_mesh);
-            }
-            for(let k=-2; k<=2; k+=4) {
-                let rail_h_sup = BABYLON.MeshBuilder.CreateBox('rail_h_sup', {height: 0.1, depth: chunk_size, width: 0.6}, scene);   //h_sup indica la parte orizzontale superiore di una rotaia
-                rail_h_sup.material = metal;
-                rail_h_sup.position.y = +0.5;
-                rail_h_sup.position.x = k + x_offset;
-                rail_h_sup.position.z = z_offset;
-                rail_h_sup.setParent(parent_mesh);
-            }
-            for(let i=-(chunk_size/2);i<=(chunk_size/2); i+=4) {
-                let traversa = BABYLON.MeshBuilder.CreateBox('traversa',{height: 0.25, depth: 1, width: 6.5}, scene);
-                traversa.material = wood;
-                traversa.position.x = x_offset;
-                traversa.position.y = -0.675;  //(-0.5-0.1/2-0.25/2)
-                traversa.position.z = i + z_offset;
-                traversa.setParent(parent_mesh);
-            }
-            for(let i=-(chunk_size/2);i<=(chunk_size/2); i+=4) {
-                for(let j=-1.7; j<=1.7; j+=3.4) {
-                    let bullone = BABYLON.MeshBuilder.CreateCylinder('bullone', {height: 0.5, diameter: 0.15}, scene);
-                    bullone.material = rusted_steel;
-                    if (x_offset < 0) bullone.position.x = j + x_offset;
-                    else bullone.position.x = j + x_offset;
-                    bullone.position.y = -0.4;
-                    bullone.position.z = i + z_offset;
-                    bullone.setParent(parent_mesh);
-                    let dado = BABYLON.MeshBuilder.CreateCylinder('dado', {height: 0.15, diameter: 0.25, tessellation: 6}, scene);
-                    dado.material = rusted_steel;
-                    if (x_offset < 0) dado.position.x = j + x_offset;
-                    else dado.position.x = j + x_offset;
-                    dado.position.y = -0.4;
-                    dado.position.z = i + z_offset;
-                    dado.setParent(parent_mesh);
-                }
-            }
-        }*/
-        
+        //creazione binari e terreno
         terrain_chunk.forEach(x => {
             let terrain = x.clone('terrain_chunk');
             terrain.position.z = z_offset;
             terrain.setParent(parent_mesh);
         });
-        
-        //creazione terreno
-        /*for(let s=-2; s<=2; s+=1) {
-            let terreno = BABYLON.MeshBuilder.CreatePlane('terreno', {size: chunk_size, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, scene);
-            terreno.material = gravel;
-            terreno.rotation.x = Math.PI/2;
-            terreno.position.x = s * chunk_size;
-            terreno.position.y = -0.8;
-            terreno.position.z = z_offset;
-            terreno.setParent(parent_mesh);
-        }*/
         
         //creazione pali
         if(z_offset % (5*chunk_size) == 0) {  //creo i pali ogni 5 chunks
@@ -252,38 +188,6 @@ function createTerrain(scene) {
                 element.setParent(parent_mesh);
             });
         }
-        
-        /*for(let i=-(chunk_size/2); i<=(chunk_size/2); i+=2) {   //sezione verticale della ringhiera
-            for(let x_offset=-48; x_offset<=48; x_offset+=96) {
-                let sbarra_v = BABYLON.MeshBuilder.CreateBox('sbarra_h', {width: 0.25, depth: 0.25, height: 10}, scene);
-                sbarra_v.material = rusted_steel;
-                sbarra_v.rotation.y = Math.PI/4;
-                sbarra_v.position.x = x_offset;
-                sbarra_v.position.y = 5 - 0.8;
-                sbarra_v.position.z = i + z_offset;   
-                sbarra_v.setParent(parent_mesh); 
-                }
-        }
-        for(let i=-2; i<=2; i+=4) { //sezione orizzontale della ringhiera
-            for(let x_offset=-48; x_offset<=48; x_offset+=96) {
-                let sbarra_h = BABYLON.MeshBuilder.CreateBox('sbarra_v', {width: 0.25, depth: 0.25, height: chunk_size}, scene);
-                sbarra_h.material = rusted_steel;
-                sbarra_h.rotation.x = Math.PI/2;
-                sbarra_h.position.x = x_offset;
-                if(i < 0) sbarra_h.position.y = i + 4;
-                else sbarra_h.position.y = i + 6;
-                sbarra_h.position.z = z_offset;
-                sbarra_h.setParent(parent_mesh);
-                }
-        }
-        for(let x_offset=-48; x_offset<=48; x_offset+=96) { //blocco di appoggio per i pali verticali della ringhiera
-            let blocco = BABYLON.MeshBuilder.CreateBox('blocco', {width: 5, depth: chunk_size, height: 1}, scene);
-            blocco.material = cemento;
-            blocco.position.x = x_offset;
-            blocco.position.y = -0.8 + 0.5;
-            blocco.position.z = z_offset;
-            blocco.setParent(parent_mesh);
-        }*/
     }
     return parent_mesh;
 }
