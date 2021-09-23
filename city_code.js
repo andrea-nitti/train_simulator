2edbf98ebbff9ceccb99c9ed880354ec9ef93f6f
Andrea Nitti <andrea.nitti2201@gmail.com>
2021-09-23 21:10:10 +0200

    Reduced arguments passed to two building functions

--- city_code.js
+++ city_code.js
@@ -1,31 +1,31 @@
 "use strict";
 function cittaP1(scene, posx, posz, arrayOfCityMeshes, cityTreesParentMesh) {
-    casaAlta(scene, posx+64, posz, arrayOfCityMeshes);
-    casaAlta(scene, posx+64, posz+80, arrayOfCityMeshes);
-    casaAlta(scene, posx+64, posz+160, arrayOfCityMeshes);
-    casaAlta(scene, posx+160, posz, arrayOfCityMeshes);
-    casaAlta(scene, posx+160, posz+80, arrayOfCityMeshes);
-    casaAlta(scene, posx+160, posz+160, arrayOfCityMeshes);
-    casaAlta(scene, posx+260, posz+324, arrayOfCityMeshes);
-    casaAlta(scene, posx+460, posz+80, arrayOfCityMeshes);
-    casaAlta(scene, posx+430, posz+160, arrayOfCityMeshes);
-    casaAlta(scene, posx+470, posz+300, arrayOfCityMeshes);
-    casaAlta(scene, posx+380, posz+420, arrayOfCityMeshes);
+    casaAlta(posx+64, posz, arrayOfCityMeshes);
+    casaAlta(posx+64, posz+80, arrayOfCityMeshes);
+    casaAlta(posx+64, posz+160, arrayOfCityMeshes);
+    casaAlta(posx+160, posz, arrayOfCityMeshes);
+    casaAlta(posx+160, posz+80, arrayOfCityMeshes);
+    casaAlta(posx+160, posz+160, arrayOfCityMeshes);
+    casaAlta(posx+260, posz+324, arrayOfCityMeshes);
+    casaAlta(posx+460, posz+80, arrayOfCityMeshes);
+    casaAlta(posx+430, posz+160, arrayOfCityMeshes);
+    casaAlta(posx+470, posz+300, arrayOfCityMeshes);
+    casaAlta(posx+380, posz+420, arrayOfCityMeshes);
 
-    casaBassa(scene, posx+70, posz+418, arrayOfCityMeshes);
-    casaBassa(scene, posx+140, posz+418, arrayOfCityMeshes);
-    casaBassa(scene, posx+208, posz+422, arrayOfCityMeshes);
-    casaBassa(scene, posx+280, posz+422, arrayOfCityMeshes);
-    casaBassa(scene, posx+266, posz+264, arrayOfCityMeshes);
-    casaBassa(scene, posx+342, posz+264, arrayOfCityMeshes);
-    casaBassa(scene, posx+340, posz+330, arrayOfCityMeshes);
-    casaBassa(scene, posx+240, posz+12, arrayOfCityMeshes);
-    casaBassa(scene, posx+302, posz+10, arrayOfCityMeshes);
-    casaBassa(scene, posx+360, posz+8, arrayOfCityMeshes);
-    casaBassa(scene, posx+420, posz+8, arrayOfCityMeshes);
-    casaBassa(scene, posx+482, posz+12, arrayOfCityMeshes);
-    casaBassa(scene, posx+478, posz+240, arrayOfCityMeshes);
-    casaBassa(scene, posx+458, posz+382, arrayOfCityMeshes);
+    casaBassa(posx+70, posz+418, arrayOfCityMeshes);
+    casaBassa(posx+140, posz+418, arrayOfCityMeshes);
+    casaBassa(posx+208, posz+422, arrayOfCityMeshes);
+    casaBassa(posx+280, posz+422, arrayOfCityMeshes);
+    casaBassa(posx+266, posz+264, arrayOfCityMeshes);
+    casaBassa(posx+342, posz+264, arrayOfCityMeshes);
+    casaBassa(posx+340, posz+330, arrayOfCityMeshes);
+    casaBassa(posx+240, posz+12, arrayOfCityMeshes);
+    casaBassa(posx+302, posz+10, arrayOfCityMeshes);
+    casaBassa(posx+360, posz+8, arrayOfCityMeshes);
+    casaBassa(posx+420, posz+8, arrayOfCityMeshes);
+    casaBassa(posx+482, posz+12, arrayOfCityMeshes);
+    casaBassa(posx+478, posz+240, arrayOfCityMeshes);
+    casaBassa(posx+458, posz+382, arrayOfCityMeshes);
 
     prato(scene, posx+140, posz+326, 128, arrayOfCityMeshes);
     prato(scene, posx+330, posz+160, 128, arrayOfCityMeshes);
@@ -43,19 +43,19 @@ function cittaP1(scene, posx, posz, arrayOfCityMeshes, cityTreesParentMesh) {
 
 function cittaP2(scene, posx, posz, arrayOfCityMeshes, cityTreesParentMesh) {
 
-    casaBassa(scene, posx+70, posz+20, arrayOfCityMeshes);
-    casaBassa(scene, posx+70, posz+100, arrayOfCityMeshes);
-    casaBassa(scene, posx+86, posz+180, arrayOfCityMeshes);
-    casaBassa(scene, posx+90, posz+260, arrayOfCityMeshes);
-    casaBassa(scene, posx+78, posz+340, arrayOfCityMeshes);
-    casaBassa(scene, posx+86, posz+420, arrayOfCityMeshes);
+    casaBassa(posx+70, posz+20, arrayOfCityMeshes);
+    casaBassa(posx+70, posz+100, arrayOfCityMeshes);
+    casaBassa(posx+86, posz+180, arrayOfCityMeshes);
+    casaBassa(posx+90, posz+260, arrayOfCityMeshes);
+    casaBassa(posx+78, posz+340, arrayOfCityMeshes);
+    casaBassa(posx+86, posz+420, arrayOfCityMeshes);
 
-    casaBassa(scene, posx+474, posz+20, arrayOfCityMeshes);
-    casaBassa(scene, posx+480, posz+100, arrayOfCityMeshes);
-    casaBassa(scene, posx+474, posz+180, arrayOfCityMeshes);
-    casaBassa(scene, posx+488, posz+260, arrayOfCityMeshes);
-    casaBassa(scene, posx+490, posz+340, arrayOfCityMeshes);
-    casaBassa(scene, posx+478, posz+420, arrayOfCityMeshes);
+    casaBassa(posx+474, posz+20, arrayOfCityMeshes);
+    casaBassa(posx+480, posz+100, arrayOfCityMeshes);
+    casaBassa(posx+474, posz+180, arrayOfCityMeshes);
+    casaBassa(posx+488, posz+260, arrayOfCityMeshes);
+    casaBassa(posx+490, posz+340, arrayOfCityMeshes);
+    casaBassa(posx+478, posz+420, arrayOfCityMeshes);
 
     prato(scene, posx+304, posz+368, 256, arrayOfCityMeshes);
     prato(scene, posx+304, posz+112, 256, arrayOfCityMeshes);
@@ -87,50 +87,50 @@ function cittaP2(scene, posx, posz, arrayOfCityMeshes, cityTreesParentMesh) {
 
 function cittaP3(scene, posx, posz, arrayOfCityMeshes, cityTreesParentMesh) {
 
-    casaAlta(scene, posx+400, posz+334, arrayOfCityMeshes);
-    casaAlta(scene, posx+400, posz+254, arrayOfCityMeshes);
-    casaAlta(scene, posx+400, posz+170, arrayOfCityMeshes);
-    casaAlta(scene, posx+400, posz+84, arrayOfCityMeshes);
-    casaAlta(scene, posx+320, posz+84, arrayOfCityMeshes);
-    casaAlta(scene, posx+236, posz+84, arrayOfCityMeshes);
-    casaAlta(scene, posx+150, posz+84, arrayOfCityMeshes);
-    casaAlta(scene, posx+150, posz+170, arrayOfCityMeshes);
-    casaAlta(scene, posx+150, posz+254, arrayOfCityMeshes);
-    casaAlta(scene, posx+150, posz+334, arrayOfCityMeshes);
-    casaAlta(scene, posx+236, posz+334, arrayOfCityMeshes);
-    casaAlta(scene, posx+320, posz+334, arrayOfCityMeshes);
+    casaAlta(posx+400, posz+334, arrayOfCityMeshes);
+    casaAlta(posx+400, posz+254, arrayOfCityMeshes);
+    casaAlta(posx+400, posz+170, arrayOfCityMeshes);
+    casaAlta(posx+400, posz+84, arrayOfCityMeshes);
+    casaAlta(posx+320, posz+84, arrayOfCityMeshes);
+    casaAlta(posx+236, posz+84, arrayOfCityMeshes);
+    casaAlta(posx+150, posz+84, arrayOfCityMeshes);
+    casaAlta(posx+150, posz+170, arrayOfCityMeshes);
+    casaAlta(posx+150, posz+254, arrayOfCityMeshes);
+    casaAlta(posx+150, posz+334, arrayOfCityMeshes);
+    casaAlta(posx+236, posz+334, arrayOfCityMeshes);
+    casaAlta(posx+320, posz+334, arrayOfCityMeshes);
 
-    casaAlta(scene, posx+54, posz-6, arrayOfCityMeshes);
+    casaAlta(posx+54, posz-6, arrayOfCityMeshes);
 
-    casaBassa(scene, posx+150, posz+0, arrayOfCityMeshes);
-    casaBassa(scene, posx+214, posz+0, arrayOfCityMeshes);
-    casaBassa(scene, posx+276, posz-2, arrayOfCityMeshes);
-    casaAlta(scene, posx+338, posz-6, arrayOfCityMeshes);
-    casaBassa(scene, posx+410, posz+0, arrayOfCityMeshes);
+    casaBassa(posx+150, posz+0, arrayOfCityMeshes);
+    casaBassa(posx+214, posz+0, arrayOfCityMeshes);
+    casaBassa(posx+276, posz-2, arrayOfCityMeshes);
+    casaAlta(posx+338, posz-6, arrayOfCityMeshes);
+    casaBassa(posx+410, posz+0, arrayOfCityMeshes);
 
-    casaBassa(scene, posx+492, posz+0, arrayOfCityMeshes);
+    casaBassa(posx+492, posz+0, arrayOfCityMeshes);
 
-    casaAlta(scene, posx+486, posz+84, arrayOfCityMeshes);
-    casaBassa(scene, posx+492, posz+154, arrayOfCityMeshes);
-    casaBassa(scene, posx+494, posz+220, arrayOfCityMeshes);
-    casaAlta(scene, posx+492, posz+280, arrayOfCityMeshes);
-    casaBassa(scene, posx+492, posz+350, arrayOfCityMeshes);
+    casaAlta(posx+486, posz+84, arrayOfCityMeshes);
+    casaBassa(posx+492, posz+154, arrayOfCityMeshes);
+    casaBassa(posx+494, posz+220, arrayOfCityMeshes);
+    casaAlta(posx+492, posz+280, arrayOfCityMeshes);
+    casaBassa(posx+492, posz+350, arrayOfCityMeshes);
 
-    casaBassa(scene, posx+492, posz+432, arrayOfCityMeshes);
+    casaBassa(posx+492, posz+432, arrayOfCityMeshes);
 
-    casaBassa(scene, posx+150, posz+432, arrayOfCityMeshes);
-    casaBassa(scene, posx+216, posz+432, arrayOfCityMeshes);
-    casaAlta(scene, posx+274, posz+426, arrayOfCityMeshes);
-    casaBassa(scene, posx+342, posz+432, arrayOfCityMeshes);
-    casaBassa(scene, posx+412, posz+434, arrayOfCityMeshes);
+    casaBassa(posx+150, posz+432, arrayOfCityMeshes);
+    casaBassa(posx+216, posz+432, arrayOfCityMeshes);
+    casaAlta(posx+274, posz+426, arrayOfCityMeshes);
+    casaBassa(posx+342, posz+432, arrayOfCityMeshes);
+    casaBassa(posx+412, posz+434, arrayOfCityMeshes);
 
-    casaBassa(scene, posx+60, posz+432, arrayOfCityMeshes);
+    casaBassa(posx+60, posz+432, arrayOfCityMeshes);
 
-    casaBassa(scene, posx+62, posz+92, arrayOfCityMeshes);
-    casaAlta(scene, posx+60, posz+150, arrayOfCityMeshes);
-    casaBassa(scene, posx+60, posz+216, arrayOfCityMeshes);
-    casaBassa(scene, posx+60, posz+280, arrayOfCityMeshes);
-    casaBassa(scene, posx+62, posz+350, arrayOfCityMeshes);
+    casaBassa(posx+62, posz+92, arrayOfCityMeshes);
+    casaAlta(posx+60, posz+150, arrayOfCityMeshes);
+    casaBassa(posx+60, posz+216, arrayOfCityMeshes);
+    casaBassa(posx+60, posz+280, arrayOfCityMeshes);
+    casaBassa(posx+62, posz+350, arrayOfCityMeshes);
 
     prato(scene, posx+300, posz+240, 128, arrayOfCityMeshes);
 
@@ -142,46 +142,46 @@ function cittaP3(scene, posx, posz, arrayOfCityMeshes, cityTreesParentMesh) {
 
 function cittaP4(scene, posx, posz, arrayOfCityMeshes, cityTreesParentMesh) {
 
-    casaBassa(scene, posx+142, posz-4, arrayOfCityMeshes);
-    casaBassa(scene, posx+202, posz-4, arrayOfCityMeshes);
-    casaBassa(scene, posx+262, posz-4, arrayOfCityMeshes);
-    casaBassa(scene, posx+320, posz-6, arrayOfCityMeshes);
-    casaBassa(scene, posx+382, posz-4, arrayOfCityMeshes);
-    casaBassa(scene, posx+442, posz-2, arrayOfCityMeshes);
-    casaBassa(scene, posx+500, posz-2, arrayOfCityMeshes);
+    casaBassa(posx+142, posz-4, arrayOfCityMeshes);
+    casaBassa(posx+202, posz-4, arrayOfCityMeshes);
+    casaBassa(posx+262, posz-4, arrayOfCityMeshes);
+    casaBassa(posx+320, posz-6, arrayOfCityMeshes);
+    casaBassa(posx+382, posz-4, arrayOfCityMeshes);
+    casaBassa(posx+442, posz-2, arrayOfCityMeshes);
+    casaBassa(posx+500, posz-2, arrayOfCityMeshes);
 
-    casaBassa(scene, posx+502, posz+90, arrayOfCityMeshes);
-    casaBassa(scene, posx+500, posz+150, arrayOfCityMeshes);
-    casaBassa(scene, posx+500, posz+210, arrayOfCityMeshes);
-    casaBassa(scene, posx+502, posz+302, arrayOfCityMeshes);
-    casaBassa(scene, posx+500, posz+358, arrayOfCityMeshes);
-    casaBassa(scene, posx+504, posz+438, arrayOfCityMeshes);
+    casaBassa(posx+502, posz+90, arrayOfCityMeshes);
+    casaBassa(posx+500, posz+150, arrayOfCityMeshes);
+    casaBassa(posx+500, posz+210, arrayOfCityMeshes);
+    casaBassa(posx+502, posz+302, arrayOfCityMeshes);
+    casaBassa(posx+500, posz+358, arrayOfCityMeshes);
+    casaBassa(posx+504, posz+438, arrayOfCityMeshes);
 
-    casaBassa(scene, posx+410, posz+92, arrayOfCityMeshes);
-    casaBassa(scene, posx+410, posz+152, arrayOfCityMeshes);
-    casaBassa(scene, posx+410, posz+210, arrayOfCityMeshes);
-    casaBassa(scene, posx+408, posz+302, arrayOfCityMeshes);
-    casaBassa(scene, posx+410, posz+360, arrayOfCityMeshes);
-    casaBassa(scene, posx+408, posz+440, arrayOfCityMeshes);
+    casaBassa(posx+410, posz+92, arrayOfCityMeshes);
+    casaBassa(posx+410, posz+152, arrayOfCityMeshes);
+    casaBassa(posx+410, posz+210, arrayOfCityMeshes);
+    casaBassa(posx+408, posz+302, arrayOfCityMeshes);
+    casaBassa(posx+410, posz+360, arrayOfCityMeshes);
+    casaBassa(posx+408, posz+440, arrayOfCityMeshes);
 
-    casaBassa(scene, posx+142, posz+92, arrayOfCityMeshes);
-    casaBassa(scene, posx+144, posz+150, arrayOfCityMeshes);
-    casaBassa(scene, posx+144, posz+208, arrayOfCityMeshes);
-    casaBassa(scene, posx+140, posz+298, arrayOfCityMeshes);
-    casaBassa(scene, posx+140, posz+362, arrayOfCityMeshes);
-    casaBassa(scene, posx+142, posz+442, arrayOfCityMeshes);
+    casaBassa(posx+142, posz+92, arrayOfCityMeshes);
+    casaBassa(posx+144, posz+150, arrayOfCityMeshes);
+    casaBassa(posx+144, posz+208, arrayOfCityMeshes);
+    casaBassa(posx+140, posz+298, arrayOfCityMeshes);
+    casaBassa(posx+140, posz+362, arrayOfCityMeshes);
+    casaBassa(posx+142, posz+442, arrayOfCityMeshes);
 
-    casaBassa(scene, posx+338, posz+300, arrayOfCityMeshes);
-    casaBassa(scene, posx+278, posz+302, arrayOfCityMeshes);
-    casaBassa(scene, posx+210, posz+302, arrayOfCityMeshes);
+    casaBassa(posx+338, posz+300, arrayOfCityMeshes);
+    casaBassa(posx+278, posz+302, arrayOfCityMeshes);
+    casaBassa(posx+210, posz+302, arrayOfCityMeshes);
 
-    casaBassa(scene, posx+342, posz+362, arrayOfCityMeshes);
-    casaBassa(scene, posx+280, posz+360, arrayOfCityMeshes);
-    casaBassa(scene, posx+210, posz+362, arrayOfCityMeshes);
+    casaBassa(posx+342, posz+362, arrayOfCityMeshes);
+    casaBassa(posx+280, posz+360, arrayOfCityMeshes);
+    casaBassa(posx+210, posz+362, arrayOfCityMeshes);
 
-    casaBassa(scene, posx+340, posz+440, arrayOfCityMeshes);
-    casaBassa(scene, posx+280, posz+442, arrayOfCityMeshes);
-    casaBassa(scene, posx+208, posz+440, arrayOfCityMeshes);
+    casaBassa(posx+340, posz+440, arrayOfCityMeshes);
+    casaBassa(posx+280, posz+442, arrayOfCityMeshes);
+    casaBassa(posx+208, posz+440, arrayOfCityMeshes);
     
     prato(scene, posx+300, posz+180, 200, arrayOfCityMeshes);
 
@@ -197,7 +197,7 @@ function cittaP4(scene, posx, posz, arrayOfCityMeshes, cityTreesParentMesh) {
 }
 
 function muro(scene, posx, posz, arrayOfCityMeshes) {
-    const muro = BABYLON.MeshBuilder.CreateBox('muro', {width: 512, height: 16, depth: 3},scene);
+    const muro = BABYLON.MeshBuilder.CreateBox('muro', {width: 512, height: 16, depth: 3}, scene);
     muro.material = bricks_rotated;
     muro.rotation.y = Math.PI/2;
     muro.position.x = posx;
@@ -206,28 +206,28 @@ function muro(scene, posx, posz, arrayOfCityMeshes) {
     arrayOfCityMeshes.push(muro);
 }
 
-function casaAlta(scene, posx, posz, arrayOfCityMeshes) {
+function casaAlta(posx, posz, arrayOfCityMeshes) {
     palazzo.forEach(x => {
-        const partePalazzo = x.clone('palazzo');
-        partePalazzo.position.x = posx+32;
-        partePalazzo.position.y = 30;
-        partePalazzo.position.z = posz+32;
-        arrayOfCityMeshes.push(partePalazzo);
+        const buildingPart = x.clone('');
+        buildingPart.position.x = posx+32;
+        buildingPart.position.y = 30;
+        buildingPart.position.z = posz+32;
+        arrayOfCityMeshes.push(buildingPart);
     });
 }
 
-function casaBassa(scene, posx, posz, arrayOfCityMeshes) {
+function casaBassa(posx, posz, arrayOfCityMeshes) {
     casa.forEach(x => {
-        const parteCasa = x.clone('casa');
-        parteCasa.position.x = posx+25;
-        parteCasa.position.y = 14;
-        parteCasa.position.z = posz+25;
-        arrayOfCityMeshes.push(parteCasa);
+        const housePart = x.clone('');
+        housePart.position.x = posx+25;
+        housePart.position.y = 14;
+        housePart.position.z = posz+25;
+        arrayOfCityMeshes.push(housePart);
     });
 }
 
 function terrenoCitta(scene, posx, posz, arrayOfCityMeshes) {
-    const terrenoCitta = BABYLON.MeshBuilder.CreatePlane('terrenoCitta', {width: 512, height: 512},scene);
+    const terrenoCitta = BABYLON.MeshBuilder.CreatePlane('terrenoCitta', {width: 512, height: 512}, scene);
     terrenoCitta.material = cemento;
     terrenoCitta.position.x = posx;
     terrenoCitta.position.y = -0.8;
@@ -237,7 +237,7 @@ function terrenoCitta(scene, posx, posz, arrayOfCityMeshes) {
 }
 
 function prato(scene, posx, posz, size, arrayOfCityMeshes) {
-    const prato = BABYLON.MeshBuilder.CreatePlane('prato', {width: size, height: size},scene);
+    const prato = BABYLON.MeshBuilder.CreatePlane('prato', {width: size, height: size}, scene);
     prato.material = erba;
     prato.position.x = posx;
     prato.position.y = 0;
@@ -270,8 +270,8 @@ function createForest(scene, posx, posz, forestParentMesh) {
     const forestTree = albero();
     forestTree.setParent(forestParentMesh);
     forestTree.thinInstanceAdd(arrayOfTrees);
-    //casaBassa(scene, posx+170, posz+380, parent_mesh);
-    //casaBassa(scene, posx+290, posz+20, parent_mesh);
+    //casaBassa(posx+170, posz+380, parent_mesh);
+    //casaBassa(posx+290, posz+20, parent_mesh);
     
     return forestTree;
 }
